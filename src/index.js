eb53091fce23e050f0f85d9333cb9a06cc94f0f9
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-10-20 08:50:39 +0500

    Add retrieved data to HTML document

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 import './style.css';
 import './stylesheets/slider.css';
+import './stylesheets/meals.css';
 import slideShow from './modules/slider.js';
+import getMeals from './modules/getMeals.js';
+
+const mealsSection = document.querySelector('.meals-section');
+const parser = new DOMParser();
 
 slideShow();
+
+let mealsList = [];
+const loadInitialData = async () => {
+  mealsList = await getMeals();
+  // loadMeals(mealsList);
+  mealsList.meals.forEach((data) => {
+    const string = `
+      <div>
+        <img src="${data.strMealThumb}" alt="meal" class="meal-img">
+        
+        <div class="meal-details">
+          <div class="meal-desc">
+            <p class="title m-0">${data.strMeal}</p>  
+            <p class="like m-0"><i class="fa-solid fa-heart"></i></p>
+          </div>
+          <button type="button" class="comment-btn">Comments</button>
+        </div>
+
+      </div>`;
+
+    const stringElement = parser.parseFromString(string, 'text/html').body.firstChild;
+    mealsSection.append(stringElement);
+  });
+};
+
+loadInitialData();
